7a97085be136e584711773cb4e1da551d4bff553
Ron <tikicoder@gmail.com>
2023-05-31 22:42:24 -0700

    updates for v1

--- threemystic_cloud_client/cloud_providers/aws/test/base.py
+++ threemystic_cloud_client/cloud_providers/aws/test/base.py
@@ -7,34 +7,6 @@ class cloud_client_aws_test_base(base):
   # There is not post init when in Config Mode
   def _post_init(self, *args, **kwargs):
     pass
-    
-  def update_config_profile(self, config, profile_name, running_config, auto_save = True, *args, **kwargs):
-    if config is None:
-      config = self._load_config()
 
-    if config.get("profiles") is None:
-      config["profiles"] = {
-      }
-    
-    if config.get("profiles").get("aws") is None:
-      config["profiles"]["aws"] = {
-      }
-
-    config["profiles"]["aws"][profile_name] = running_config
-    if auto_save:
-      self.save_config(config= config)
-
-    return config
-  
-  def save_config(self, config, *args, **kwargs):
-     self.config_path().write_text(
-      data= self.get_common().helper_yaml().dumps(data= config)
-     )
-     
   def step(self, config, *args, **kwargs):
-    
-    if self.is_cli_installed(config= config) != True:
-      print("aws cli is not marked as installed please start over")
-      return False
-
     return True
