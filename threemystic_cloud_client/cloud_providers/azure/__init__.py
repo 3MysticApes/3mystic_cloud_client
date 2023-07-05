18be5afbecf192b7a85ad04038c14dd422555d26
Ron <tikicoder@gmail.com>
2023-07-04 22:55:16 -0700

    updates

--- threemystic_cloud_client/cloud_providers/azure/__init__.py
+++ threemystic_cloud_client/cloud_providers/azure/__init__.py
@@ -7,13 +7,22 @@ class cloud_client_azure(base):
   def _login(self, *args, **kwargs):
     pass
   
-  def action_test(self, *args, **kwargs):
+  def action_test(self, *args, **kwargs):    
+    if not self.is_provider_config_completed():
+      print("Provider must be configured first")
+      self._setup_another_config()
+      return
+    
     from threemystic_cloud_client.cloud_providers.azure.test.step_1 import cloud_client_azure_test_step_1 as test
     next_step = test(common= self.get_common(), logger= self.get_logger(), *args, **kwargs)
     
     next_step.step()
   
-  def action_config(self, *args, **kwargs): 
+  def action_config(self, *args, **kwargs):     
+    if not self.is_provider_config_completed():
+      print("Provider must be configured first")
+      self._setup_another_config()
+      return
     
     from threemystic_cloud_client.cloud_providers.azure.config.step_1 import cloud_client_azure_config_step_1 as step
     next_step = step(common= self.get_common(), logger= self.get_logger())
